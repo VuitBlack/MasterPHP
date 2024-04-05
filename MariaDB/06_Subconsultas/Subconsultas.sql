89fa71444b1a88f42021e17c53ed962b543f0bcb
ricardo <ricardo@localhost>
2024-04-05 19:49:24 +0200

    Sección 34 - Subconsultas Videos 130, 131, 132 y 133.

--- MariaDB/06_Subconsultas/Subconsultas.sql
+++ MariaDB/06_Subconsultas/Subconsultas.sql
@@ -10,6 +10,9 @@
 
 # Insertamos un nuevo usario en la tabla usuarios.
 INSERT INTO usuarios VALUES(null, 'Admin', 'Admin', 'admin@admin.com', 'admin', CURDATE());
+INSERT INTO entradas VALUES(null, 3, 1, 'Guía de GTA Vice City', 'GTA', CURDATE());
+INSERT INTO entradas VALUES(null, 1, 1, 'Jugando con SQL', 'SQL', CURDATE());
+INSERT INTO categorias VALUES(null, 'Plataformas');
 
 # Visualizamos toda la tabla usuarios
 SELECT * FROM usuarios;
@@ -19,3 +22,28 @@ SELECT * FROM usuarios WHERE id IN (SELECT usuario_id FROM entradas);
 
 # Mostar todos los usuarios que NO tengan entradas en la tabla entradas.
 SELECT * FROM usuarios WHERE id NOT IN (SELECT usuario_id FROM entradas);
+
+# Mostrar a los usuarios que en alguna entrada hablen de GTA.
+SELECT nombre, apellidos FROM usuarios WHERE id IN
+    (SELECT usuario_id FROM entradas WHERE titulo LIKE "%GTA%");
+
+# Mostrar todas las entradas de la categoría acción usando su nombre.
+SELECT categoria_id, titulo FROM entradas WHERE categoria_id IN
+    (SELECT id FROM categorias WHERE nombre='Acción');
+
+# Mostrar las categorias con 3 o más entradas.
+SELECT nombre FROM categorias WHERE id IN
+    (SELECT categoria_id FROM entradas GROUP BY categoria_id HAVING COUNT(categoria_id)>=3);
+
+# Mostrar los usuarios que crearon una entrada un martes. "Los días de la semana empiezan en domingo=1".
+SELECT nombre, apellidos FROM usuarios WHERE id IN
+    (SELECT usuario_id FROM entradas WHERE DAYOFWEEK(fecha)=6);
+
+# Mostrar el nombre del usuario con más entradas.
+SELECT nombre, apellidos FROM usuarios WHERE id =
+    (SELECT usuario_id FROM entradas GROUP BY usuario_id ORDER BY COUNT(id) DESC LIMIT 1);
+
+# Mostrar las categorías sin entradas.
+SELECT * FROM categorias WHERE id NOT IN
+    (SELECT categoria_id FROM entradas);
+
