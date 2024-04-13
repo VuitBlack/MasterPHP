570d791d58e5a7633051e674199a05a14f563f67
VuitBlack <rsandovalp@uoc.edu>
2024-04-13 21:05:09 +0200

    Sección 41: Estructura HTML de la aplicación Vídeo: 182.

---
+++ Proyecto-PHP/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog de Videojuegos</title>
+    </head>
+    <body>
+        <!--Head-->
+        <header>
+            <div id="logo">
+                <a href="index.php">
+                    Blog de Videojuegos
+                </a>
+            </div>
+        </header>
+        <!--Menú-->
+
+        <!--Sidebar-->
+
+        <!--Main Content-->
+
+        <!--Foot-->
+    </body>
+</html>
