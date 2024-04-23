ba1c2fa47350ec7574ce90b3a24c23487139cfdc
ricardo <ricardo@localhost>
2024-04-23 20:21:06 +0200

    Sección 42 - Maquetación y estilos con CSS Videos 189.

--- Proyecto-PHP/index.php
+++ Proyecto-PHP/index.php
@@ -83,33 +83,53 @@
             <!--Main Content-->
             <div id="principal">
                 <h1>Últimas entradas</h1>
+                
                 <article class="entrada">
-                    <h2>Título de mi entrada</h2>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in fringilla nibh, ut dapibus lectus. 
-                    Nullam commodo quam vel massa malesuada, a iaculis elit imperdiet. Integer vulputate nisi a massa dapibus mattis. 
-                    Suspendisse potenti. Morbi et commodo lorem, ultrices pharetra urna.
-                    </p>
-                    <h2>Título de mi entrada</h2>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in fringilla nibh, ut dapibus lectus. 
-                    Nullam commodo quam vel massa malesuada, a iaculis elit imperdiet. Integer vulputate nisi a massa dapibus mattis. 
-                    Suspendisse potenti. Morbi et commodo lorem, ultrices pharetra urna.
-                    </p>
-                    <h2>Título de mi entrada</h2>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in fringilla nibh, ut dapibus lectus. 
-                    Nullam commodo quam vel massa malesuada, a iaculis elit imperdiet. Integer vulputate nisi a massa dapibus mattis. 
-                    Suspendisse potenti. Morbi et commodo lorem, ultrices pharetra urna.
-                    </p>
-                    <h2>Título de mi entrada</h2>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in fringilla nibh, ut dapibus lectus. 
-                    Nullam commodo quam vel massa malesuada, a iaculis elit imperdiet. Integer vulputate nisi a massa dapibus mattis. 
-                    Suspendisse potenti. Morbi et commodo lorem, ultrices pharetra urna.
-                    </p>
+                    <a href="">
+                        <h2>Título de mi entrada</h2>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in fringilla nibh, ut dapibus lectus. 
+                        Nullam commodo quam vel massa malesuada, a iaculis elit imperdiet. Integer vulputate nisi a massa dapibus mattis. 
+                        Suspendisse potenti. Morbi et commodo lorem, ultrices pharetra urna.
+                        </p>
+                    </a>
                 </article>
+           
+                <article class="entrada">
+                    <a href="">
+                        <h2>Título de mi entrada</h2>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in fringilla nibh, ut dapibus lectus. 
+                        Nullam commodo quam vel massa malesuada, a iaculis elit imperdiet. Integer vulputate nisi a massa dapibus mattis. 
+                        Suspendisse potenti. Morbi et commodo lorem, ultrices pharetra urna.
+                        </p>
+                    </a>
+                </article>
+                
+                <article class="entrada">
+                    <a href="">
+                        <h2>Título de mi entrada</h2>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in fringilla nibh, ut dapibus lectus. 
+                        Nullam commodo quam vel massa malesuada, a iaculis elit imperdiet. Integer vulputate nisi a massa dapibus mattis. 
+                        Suspendisse potenti. Morbi et commodo lorem, ultrices pharetra urna.
+                        </p>
+                    </a>
+                </article>
+
+                <article class="entrada">
+                    <a href="">
+                        <h2>Título de mi entrada</h2>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in fringilla nibh, ut dapibus lectus. 
+                        Nullam commodo quam vel massa malesuada, a iaculis elit imperdiet. Integer vulputate nisi a massa dapibus mattis. 
+                        Suspendisse potenti. Morbi et commodo lorem, ultrices pharetra urna.
+                        </p>
+                    </a>
+                </article>
+           
             </div>
+            <div class="clearfix"></div>
         </div>
         <!--Foot-->
         <footer id="pie">
