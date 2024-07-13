8a943658654d03dfc9d1526825e0afb00b3337b1
ricardo <ricardo@localhost>
2024-07-13 19:50:32 +0200

    Vídeo 263,  CSS del Sidebar. Sección 65: Maquetación Web del proyecto (HTML y CSS).

--- Proyecto-PHP-POO-MVC/index.php
+++ Proyecto-PHP-POO-MVC/index.php
@@ -46,6 +46,7 @@
             <!-- SIDE BAR -->
             <aside id="lateral">
                 <div id="login" class="block_aside">
+                    <h3>Accede a la WEB</h3>
                     <form action="#" method="post">
                         <label for="email">Email</label>
                         <input type="email" name="email" />
@@ -53,18 +54,26 @@
                         <input type="password" name="password" />
                         <input type="submit" value="Enviar" />
                     </form>
-                    <div id="icono">
-                        <img src="Assets/img/usuario_ico.png" alt="Usuario_icono">
-                        <a href="#">Mis Pedidos</a>
-                    </div>
-                    <div id="icono">
-                        <img src="Assets/img/gestion_ped_ico.png" alt="Pedido_icono">
-                        <a href="#">Gestionar Pedidos</a>
-                    </div>
-                    <div id="icono">
-                        <img src="Assets/img/candado_ico.png" alt="Categorias_icono">
-                        <a href="#">Gestionar Categorías</a>
-                    </div>
+                    <ul>
+                        <li>
+                            <div id="icono">
+                                <img src="Assets/img/usuario_ico.png" alt="Usuario_icono">
+                                <a href="#">Mis Pedidos</a>
+                            </div>
+                        </li>
+                        <li>
+                            <div id="icono">
+                                <img src="Assets/img/gestion_ped_ico.png" alt="Pedido_icono">
+                                <a href="#">Gestionar Pedidos</a>
+                            </div>
+                        </li>
+                        <li>
+                            <div id="icono">
+                                <img src="Assets/img/candado_ico.png" alt="Categorias_icono">
+                                <a href="#">Gestionar Categorías</a>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </aside>
 
