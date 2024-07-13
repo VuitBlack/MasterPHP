6c56541a9592494dcb06e4473387135a27f7e781
ricardo <ricardo@localhost>
2024-07-13 20:27:37 +0200

    Vídeo 264,  CSS del Main Content. Sección 65: Maquetación Web del proyecto (HTML y CSS).

--- Proyecto-PHP-POO-MVC/index.php
+++ Proyecto-PHP-POO-MVC/index.php
@@ -79,23 +79,24 @@
 
             <!-- MAIN BODY -->
             <div id="central">
+                <h1>Productos destacados</h1>
                 <div class="product">
                     <img src="Assets/img/Camiseta_2.png" />
                     <h2>Camiseta de Manta Oceánica</h2>
                     <p>30€</p>
-                    <a href="#">Comprar</a>
+                    <a href="#" class="button">Comprar</a>
                 </div>
                 <div class="product">
                     <img src="Assets/img/Camiseta_1.png" />
                     <h2>Camiseta de Pelícano</h2>
                     <p>30€</p>
-                    <a href="#">Comprar</a>
+                    <a href="#" class="button">Comprar</a>
                 </div>
                 <div class="product">
                     <img src="Assets/img/Camiseta_3.png" />
                     <h2>Camiseta de Pingüino</h2>
                     <p>30€</p>
-                    <a href="#">Comprar</a>
+                    <a href="#" class="button">Comprar</a>
                 </div>
 
             </div>
