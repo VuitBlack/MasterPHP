938801be111aef5715a8090ff437089ec5c9b3ae
ricardo <ricardo@localhost>
2024-07-18 18:25:14 +0200

    Vídeo 269, Formulario de registro de usuarios Sección 67: Registro de usuarios (php, por, mvc, mysql).

--- Proyecto-PHP-POO-MVC/controllers/UsuarioController.php
+++ Proyecto-PHP-POO-MVC/controllers/UsuarioController.php
@@ -4,6 +4,14 @@ class usuarioController{
     public function index(){
         echo "Controlador de USUARIO acción INDEX";
     }
+    public function registro(){
+        require_once 'views/usuario/registro.php';
+    }
+    public function save(){
+        if(isset($_POST)){
+            var_dump($_POST);
+        }
+    }
 }
 
 ?>
