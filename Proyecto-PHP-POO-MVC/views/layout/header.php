ce37519a1f406dd7ff700bfbbc45f03ab10035ae
ricardo <ricardo@localhost>
2024-10-04 21:22:42 +0200

    Vídeo 278. Menú de categorías. Sección 69: Categorías de la tienda.

--- Proyecto-PHP-POO-MVC/views/layout/header.php
+++ Proyecto-PHP-POO-MVC/views/layout/header.php
@@ -19,26 +19,17 @@
             </div>
         </header>
         <!-- MENU -->
+         <?php $categorias=Utils::showCategorias(); ?>
         <nav id="menu">
             <ul>
                 <li>
-                    <a href="#">Inicio</a>
+                    <a href="<?=base_url?>">Inicio</a>
                 </li>
-                <li>
-                    <a href="#">Categoría 1</a>
-                </li>
-                <li>
-                    <a href="#">Categoría 2</a>
-                </li>
-                <li>
-                    <a href="#">Categoría 3</a>
-                </li>
-                <li>
-                    <a href="#">Categoría 4</a>
-                </li>
-                <li>
-                    <a href="#">Categoría 5</a>
-                </li>
+                <?php while ($cat = $categorias->fetch_object()): ?>
+                    <li>
+                        <a href="#"><?=$cat->nombre?></a>
+                    </li>
+                <?php endwhile; ?>
             </ul>
         </nav>
 
