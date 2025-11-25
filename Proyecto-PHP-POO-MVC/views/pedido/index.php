1e00c701a7098e9766f55ec42600bebbe16dd2f7
VuitBlack <r.sandovalperez@gmail.com>
2025-11-25 19:16:52 +0100

    Vídeo 297. Creo el formulario de pedido dentro de la vista del pedido.
    Y se envía toda la información del formulario al controlador por método POST.

--- Proyecto-PHP-POO-MVC/views/pedido/index.php
+++ Proyecto-PHP-POO-MVC/views/pedido/index.php
@@ -1,9 +1,29 @@
 <?php if(isset($_SESSION['identity'])): ?>
 <h1>Realizar Pedido</h1>
-<a href="<?=base_url?>carrito/index" class="button">Volver al carrito</a>
+<p>
+    <a href="<?=base_url?>carrito/index">Volver al carrito</a>
+</p>
+<br/>
+<h3>Datos del envío</h3>
+<form action="<?=base_url?>pedido/add" method="POST">
+    <p>
+        <label for="provincia">Provincia</label>
+        <input type="text" name="provincia" required>
+    </p>
+    <p>
+        <label for="localidad">Localidad</label>
+        <input type="text" name="localidad" required>
+    </p>
+    <p>
+        <label for="direccion">Dirección</label>
+        <input type="text" name="direccion" required>
+    </p>
+    <input type="submit" value="Realizar Pedido">
+</form>
+
 <?php else: ?>
 <h1>Necesitas estar identificado</h1>
-<p>Para hacer un pedido necesitas identificarte en este Website.</p></br
+<p>Para hacer un pedido necesitas identificarte en este Website.</p></br>
 <p>Por favor, identificate en el area de login o registrate en el area de registro.</p>
 
 <?php endif; ?>
