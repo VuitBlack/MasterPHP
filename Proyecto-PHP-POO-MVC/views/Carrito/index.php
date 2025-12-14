44ac97dfdca598fa753bd029518ec11fdaf9526d
VuitBlack <r.sandovalperez@gmail.com>
2025-12-14 13:41:21 +0100

    Vídeo 306. Control productos del carrito. Se añaden los métdos UP() & DOWN() en el CarritoController.php los cuales añaden o disminuyen unidades al producto comprado en el carrito. Se modifica la vista del carrito el archivo index.php para añadir los botone de sumar o restar productos. Se adecua la logica de index.php del carrito para que si no hay productos en el carrito cambie la vista a la pantalla de carrito vacío. Se añaden los estilos CSS necesarios en styles.css para adecuar los botones al espacio y posición.

--- Proyecto-PHP-POO-MVC/views/Carrito/index.php
+++ Proyecto-PHP-POO-MVC/views/Carrito/index.php
@@ -1,48 +1,57 @@
 <h1>Carrito de la compra</h1>
 
-<table class>
-    <tr>
-        <th>Imagen</th>
-        <th>Nombre</th>
-        <th>Precio</th>
-        <th>Unidades</th>
-        <th>Eliminar</th>
-    </tr>
-    <?php foreach ($carrito as $indice => $elemento):
-        $producto = $elemento['producto'];
-    ?>
+<?php if (isset($_SESSION['carrito']) && count($_SESSION['carrito']) >= 1): ?>
+
+    <table class>
         <tr>
-            <td>
-                <?php if ($producto->imagen != null): ?>
-                    <img src="<?= base_url ?>uploads/images/<?= $producto->imagen ?>" class="img_carrito"/>
-                <?php else: ?>
-                    <img src="<?= base_url ?>Assets/img/Cubo_Rubik.png" class="img_carrito"/>
-                <?php endif; ?>
-            </td>
-            <td>
-                <a href="<?= base_url?>producto/ver&id=<?=$producto->id?>"><?=$producto->nombre?></a>
-            </td>
-            <td>
-                <?=$producto->precio?>
-            </td>
-            <td>
-                <?=$elemento['unidades']?>
-            </td>
-            <td>
-                <a href="<?=base_url?>carrito/delete&index=<?=$indice?>" class="button button-del-producto button-red">Eliminar</a>
-            </td>
-
+            <th>Imagen</th>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Unidades</th>
+            <th>Eliminar</th>
         </tr>
-    <?php endforeach; ?>
-</table>
-<br/>
+        <?php foreach ($carrito as $indice => $elemento):
+            $producto = $elemento['producto'];
+        ?>
+            <tr>
+                <td>
+                    <?php if ($producto->imagen != null): ?>
+                        <img src="<?= base_url ?>uploads/images/<?= $producto->imagen ?>" class="img_carrito"/>
+                    <?php else: ?>
+                        <img src="<?= base_url ?>Assets/img/Cubo_Rubik.png" class="img_carrito"/>
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <a href="<?= base_url?>producto/ver&id=<?=$producto->id?>"><?=$producto->nombre?></a>
+                </td>
+                <td>
+                    <?=$producto->precio?>
+                </td>
+                <td>
+                    <?=$elemento['unidades']?>
+                    <div class="button-updown">
+                        <a href="<?=base_url?>carrito/up&index=<?=$indice?>" class="button">+</a>
+                        <a href="<?=base_url?>carrito/down&index=<?=$indice?>" class="button">-</a>
+                    </div>
+                </td>
+                <td>
+                    <div class="button-delete-producto">
+                        <a href="<?=base_url?>carrito/delete&index=<?=$indice?>" class="button button-red">Eliminar</a>
+                    </div>
+                </td>
 
-<div class="button-delete-carrito">
-    <a href="<?=base_url?>carrito/delete_all" class="button button-delete button-red">Vaciar Carrito</a>
-</div>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+    <br/>
 
-<div class="total-carrito">
-    <?php $stats = Utils::statsCarrito(); ?>
-    <h3>Precio total: <?=$stats['total']?> €</h3>
-    <a href="<?=base_url?>pedido/index" class="button button-pedido">Hacer Pedido</a>
-</div>
+    <div class="button-delete-carrito">
+        <a href="<?=base_url?>carrito/delete_all" class="button button-delete button-red">Vaciar Carrito</a>
+    </div>
+    <div class="total-carrito">
+        <?php $stats = Utils::statsCarrito(); ?>
+        <h3>Precio total: <?=$stats['total']?> €</h3>
+        <a href="<?=base_url?>pedido/index" class="button button-pedido">Hacer Pedido</a>
+    </div>
+
+<?php endif; ?>
