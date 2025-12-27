7e1ff5dcd507eb9aa58d2398bed7c0517b58254b
VuitBlack <r.sandovalperez@gmail.com>
2025-12-27 19:50:42 +0100

    Vídeo 320. Instalar Laravel. Vídeo 321. Instalar extensiones necesarias de PHP. Vídeo 322. Crear un HOST virtual. Vídeo 323. Estructura de los proyectos en Laravel.
    Vídeo 324. Rutas Básicas en Laravel.
    Creamos la vista mostrarFecha.blade.php que contiene el método para obtener la fecha
    y recíbe una variable $titulo, la cual tiene el título de la págnina.
    En la ruta web.php usamos un método get para imprimir la fecha llamando a la vista correspondiente.

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    echo "<h1>Hola Mundo</h1>";
+});
+
+/*
+GET: Obtener datos
+POST: Enviar datos
+PUT: Actualizar datos
+DELETE: Eliminar datos
+*/
+
+Route::get('mostrar_fecha', function () {
+    $titulo = "Mostar la fecha actual: ";
+    $fecha = date('d/m/Y');
+    return view('mostrarFecha', $arrayName = array(
+        'titulo' => $titulo
+        )
+    );
 });
