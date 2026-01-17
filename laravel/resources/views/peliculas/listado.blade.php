5558344e1f31679633ad65528dc6346c7eb185db
VuitBlack <r.sandovalperez@gmail.com>
2026-01-17 22:08:23 +0100

    Vídeo 328. Vistas en Laravel. Como enviar parámetros a la vista mediante un "array" o con la funcion "with"

---
+++ laravel/resources/views/peliculas/listado.blade.php
@@ -0,0 +1,7 @@
+<h1><?=$titulo?></h1>
+<h2>Películas</h2>
+<ul>
+    <?php foreach ($peliculas as $pelicula) { ?>
+        <li><?=$pelicula?></li>
+    <?php } ?>
+</ul>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -41,3 +41,36 @@ Route::get('/tituloLibro/{titulo}/{year?}', function ($titulo, $year = 2025) {
     'year' => '[0-9]{4}'
     )
 );
+
+Route::get('/listado-peliculas', function () {
+
+    $titulo = "Listado de películas";
+    $peliculas = array(
+        'Matrix',
+        'El Padrino',
+        'Reservoir Dogs',
+        'Django Unchained',
+        'Pulp Fiction'
+    );
+  return view('peliculas.listado', array(
+    'titulo' => $titulo,
+    'peliculas' => $peliculas
+    )
+  );
+});
+
+// Con with se puede enviar variables a la vista igual que con array
+Route::get('/listado-peliculas-2', function () {
+
+    $titulo = "Listado de películas 2";
+    $peliculas = array(
+        'Iron Man',
+        'Spiderman',
+        'Avengers',
+        'X-Men',
+        'Guardianes de la Galaxia'
+    );
+  return view('peliculas.listado')
+    ->with('titulo', $titulo)
+    ->with('peliculas', $peliculas);
+});
