534b1397d82d6e53cdd9d941f26c2989d561dccd
VuitBlack <r.sandovalperez@gmail.com>
2026-03-19 19:12:23 +0100

    Vídeo 332. IF en Blade. Se muestra un ejemplo de la estructura de control IF en Blade dentro del archivo listado.blade.php

--- laravel/resources/views/peliculas/listado.blade.php
+++ laravel/resources/views/peliculas/listado.blade.php
@@ -1,3 +1,4 @@
+<!-- IMPRIMIR POR PANTALL -->
 <h1><?=$titulo?></h1>
 <h2>Películas</h2>
 <ul>
@@ -5,7 +6,25 @@
         <li><?=$pelicula?></li>
     <?php } ?>
 </ul>
+<p>{{date ('Y')}}</p>
 
+<!-- COMENTARIOS-->
 <!-- Esto es un comentario html-->
  <?php /* Esto es un comentario en php */ ?>
 {{-- Esto es un comentario en Blade --}}
+
+<!-- CONDICIONALES (mostrar si existe la variable)-->
+{{-- Condicion ternaria PHP --}}
+<?= isset($director) ? $director : '<b><i>No hay director</i></b><br/>' ?>
+
+{{-- Condicion ternaria BLADE --}}
+{{ $director ?? 'No hay director' }}
+
+<!-- CONDICIONALES-->
+ @if($titulo && count($peliculas) >= 7)
+   <h1>El título es: "{{$titulo}}" y el listado es mayor a 7 películas</h1>
+ @elseif($titulo) 
+    <h1>El título existe pero no hay 7 películas</h1>
+ @else
+    <h1>El título no existe<h1>
+@endif
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,44 +3,45 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    //return view('welcome');
+    return view('welcome');
+});
+
+Route::get('Hello_World', function () {
     echo "<h1>Hola Mundo</h1>";
     echo "<h2>Este es mi primer proyecto con Laravel 12</h2>";
 });
 
-/* 
-Métodos http:
-    GET: Obtener datos
-    POST: Enviar datos
-    PUT: Actualizar datos
-    DELETE: Eliminar datos
-*/
+/*  Métodos http:
+ GET: Obtener datos
+ POST: Enviar datos
+ PUT: Actualizar datos
+ DELETE: Eliminar datos */
 
 Route::get('mostrar_fecha', function () {
     $titulo = "Mostar la fecha actual: ";
     return view('mostrarFecha', $arrayName = array(
-        'titulo' => $titulo
-        )
+    'titulo' => $titulo
+    )
     );
 });
 
 Route::get('/pelicula/{titulo?}', function ($titulo = "Sin película seleccionada") {
     return view('pelicula', array(
-        'titulo' => $titulo
-        )
+    'titulo' => $titulo
+    )
     );
 });
 
 Route::get('/tituloLibro/{titulo}/{year?}', function ($titulo, $year = 2025) {
     return view('tituloLibro', array(
-        'titulo' => $titulo,
-        'year' => $year
-        )
+    'titulo' => $titulo,
+    'year' => $year
+    )
     );
 })->where(array(
     'tituloLibro' => '[A-Za-z]+',
     'year' => '[0-9]{4}'
-    )
+)
 );
 
 Route::get('/listado-peliculas', function () {
@@ -53,11 +54,11 @@ Route::get('/listado-peliculas', function () {
         'Django Unchained',
         'Pulp Fiction'
     );
-  return view('peliculas.listado', array(
+    return view('peliculas.listado', array(
     'titulo' => $titulo,
     'peliculas' => $peliculas
     )
-  );
+    );
 });
 
 // Con with se puede enviar variables a la vista igual que con array
@@ -71,8 +72,7 @@ Route::get('/listado-peliculas-2', function () {
         'X-Men',
         'Guardianes de la Galaxia'
     );
-  return view('peliculas.listado_2')
+    return view('peliculas.listado_2')
     ->with('titulo', $titulo)
     ->with('peliculas', $peliculas);
 });
-
