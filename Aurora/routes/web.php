caa4ba1401cf9dcbc4d42416e2d49cb016f7aed3
VuitBlack <r.sandovalperez@gmail.com>
2026-08-15 18:05:10 +0200

    Vídeo 361: Primeras consultas con el ORM, probando desde routes/web.php para verificar que todo funciona correctamente.

--- Aurora/routes/web.php
+++ Aurora/routes/web.php
@@ -1,8 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Image;
 
 Route::get('/', function () {
+
+    $images = Image::all(); // Obtenemos todas las imágenes de la base de datos
+    foreach ($images as $image) {
+        // Aquí puedes hacer algo con cada imagen, como mostrar su nombre o ruta
+        echo '<strong>Nombre imagen: </strong>' . $image->image_path . '<br>'; // Por ejemplo, mostramos la ruta de la imagen
+        echo '<strong>Descripción: </strong>' . $image->description . '<br>'; // Mostramos la descripción de la imagen
+        echo '<strong>Subido por: </strong>' . $image->user->name .' '. $image->user->surname . '<br>'; // Mostramos el nombre del usuario que subió la imagen
+        if($image->comments) {
+            echo '<strong>Comentarios: </strong><br>'; // Mostramos un título para los comentarios
+            foreach ($image->comments as $comment) {
+                echo '<strong>Por: </strong>' . $comment->user->name .' '. $comment->user->surname . ':  '; // Mostramos el nombre del usuario que hizo el comentario
+                echo $comment->content . '<br>'; // Mostramos el contenido del comentario
+                
+            }
+        } else {
+            echo '<strong>Comentario: </strong> No hay comentarios para esta imagen.<br>';
+        }
+
+        echo 'LIKES: ' . $image->likes->count() . '<br>'; // Mostramos la cantidad de likes de la imagen
+
+        echo '<hr>'; // Separador entre imágenes
+    }
+    die(); // Detenemos la ejecución para que no se cargue la vista 'welcome'
+
     return view('welcome');
 });
 
